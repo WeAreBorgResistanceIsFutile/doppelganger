f98e4c07cb6f60619e81e5a63bb56699929b1653
Halász Levente <levente.halasz@windowslive.com>
2019-03-31 22:13:01 +0200

    Solution created. First bytes layed down.

---
+++ Doppelganger/Doppelganger.Files/FileData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doppelganger.Files
+{
+    public class FileData
+    {
+        public string Name { get; }
+        public string FullPath { get; }
+        public int Hash{ get; }
+
+        public FileData(string fileName, string fullPath, int hashCode)
+        {
+            Name = fileName;
+            FullPath = fullPath;
+            Hash = hashCode;
+        }
+    }
+}
---
+++ Doppelganger/Doppelganger.Files/FileDataExtractor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Doppelganger.Files
+{
+    public class FileDataExtractor
+    {
+        public FileData GetFileData(string fileName, string filePath, byte[] content)
+        {
+            int hashCode = GetHashCode(content);
+            return new FileData(fileName: fileName, fullPath: Path.Combine(filePath, fileName), hashCode: hashCode);
+        }
+
+        private static int GetHashCode<T>(T obj)
+        {
+            unchecked
+            {
+                const int HashingBase = (int)2166136261;
+                const int HashingMultiplier = 16777619;
+
+                int hash = HashingBase;
+
+                hash = (hash * HashingMultiplier) ^ (!(obj is null) ? obj.GetHashCode() : 0);
+                return hash;
+            }
+        }
+
+        private static int GetHashCode<T>(IEnumerable<T> obj)
+        {
+            unchecked
+            {
+                const int HashingBase = (int)2166136261;
+                const int HashingMultiplier = 16777619;
+
+                int hash = HashingBase;
+
+                var enumerator = obj?.GetEnumerator();
+
+                if(!(enumerator is null))
+                {
+                    while(enumerator.MoveNext())
+                    {
+                        hash = (hash * HashingMultiplier) ^ (!(enumerator.Current is null) ? enumerator.Current.GetHashCode() : 0);
+                    }
+                }
+                return hash;
+            }
+        }
+    }
+}
