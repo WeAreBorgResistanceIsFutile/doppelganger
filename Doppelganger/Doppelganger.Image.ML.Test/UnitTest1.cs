263681a58ae4128193e4d52354be9c2579777289
Halász Levente <levente.halasz@windowslive.com>
2019-04-19 09:37:37 +0200

    Changing ML.Test platform

---
+++ Doppelganger/Doppelganger.Image.ML.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tensorflow;
+
+namespace Doppelganger.Image.ML.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var tensor1 = tf.constant(8); // int
+            var tensor2 = tf.constant(6.0f); // float
+            var tensor3 = tf.constant(6.0); // double
+        }
+    }
+}
