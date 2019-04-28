7ffa4dd968a56e8b840e720d06e05214a6a241c5
Halász Levente <levente.halasz@windowslive.com>
2019-04-28 08:00:48 +0200

    Image feature extraction added

--- Doppelganger/Doppelganger.Image.TF/Doppelganger.Image.TF.py
+++ Doppelganger/Doppelganger.Image.TF/Doppelganger.Image.TF.py
@@ -20,80 +20,54 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
-ap.add_argument("-model", "--model", type=str, default="vgg16",
-	help="name of pre-trained network to use")
+
 args = vars(ap.parse_args())
 
-# define a dictionary that maps model names to their classes
-# inside Keras
-MODELS = {
-	"vgg16": VGG16,
-	"vgg19": VGG19,
-	"inception": InceptionV3,
-	"xception": Xception, # TensorFlow ONLY
-	"resnet": ResNet50
-}
+def extract_features(model, image, preprocess):    
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = preprocess(image)    
+    preds = model.predict(image)    
+    return preds.flatten()
 
-# esnure a valid model name was supplied via command line argument
-if args["model"] not in MODELS.keys():
-	raise AssertionError("The --model command line argument should "
-		"be a key in the `MODELS` dictionary")
+def load_image_224(imagePath): 
+    inputShape = (224, 224)
+    preprocess = imagenet_utils.preprocess_input
+    image224 = load_img(imagePath, target_size=inputShape)
+    return (image224, preprocess)
+
+def load_image_299(imagePath): 
+    inputShape = (299, 299)
+    preprocess = preprocess_input
+    image = load_img(imagePath, target_size=inputShape)
+    return (image, preprocess)
 
 # initialize the input image shape (224x224 pixels) along with
 # the pre-processing function (this might need to be changed
 # based on which model we use to classify our image)
-inputShape = (224, 224)
-preprocess = imagenet_utils.preprocess_input
 
-# if we are using the InceptionV3 or Xception networks, then we
-# need to set the input shape to (299x299) [rather than (224x224)]
-# and use a different image processing function
-if args["model"] in ("inception", "xception"):
-	inputShape = (299, 299)
-	preprocess = preprocess_input
 
-# load our the network weights from disk (NOTE: if this is the
-# first time you are running this script for a given network, the
-# weights will need to be downloaded first -- depending on which
-# network you are using, the weights can be 90-575MB, so be
-# patient; the weights will be cached and subsequent runs of this
-# script will be *much* faster)
-print("[INFO] loading {}...".format(args["model"]))
-Network = MODELS[args["model"]]
-model = Network(weights="imagenet")
+(image224,preprocess) = load_image_224(args["image"])
+(image299,preprocess) = load_image_299(args["image"])
 
-# load the input image using the Keras helper utility while ensuring
-# the image is resized to `inputShape`, the required input dimensions
-# for the ImageNet pre-trained network
-print("[INFO] loading and pre-processing image...")
-image = load_img(args["image"], target_size=inputShape)
-image = img_to_array(image)
+model = VGG16(weights='imagenet', include_top=False, pooling='avg')
+features = extract_features(model, image224, preprocess);
+print(features)
+model = VGG19(weights='imagenet', include_top=False, pooling='avg')
+features = extract_features(model, image224, preprocess);
+print(features)
+model = ResNet50(weights='imagenet', include_top=False, pooling='avg')
+features = extract_features(model, image224, preprocess);
+print(features)
 
-# our input image is now represented as a NumPy array of shape
-# (inputShape[0], inputShape[1], 3) however we need to expand the
-# dimension by making the shape (1, inputShape[0], inputShape[1], 3)
-# so we can pass it through thenetwork
-image = np.expand_dims(image, axis=0)
 
-# pre-process the image using the appropriate function based on the
-# model that has been loaded (i.e., mean subtraction, scaling, etc.)
-image = preprocess(image)
+model = Xception(weights='imagenet', include_top=False, pooling='avg')
+features = extract_features(model, image299, preprocess);
+print(features)
 
-# classify the image
-print("[INFO] classifying image with '{}'...".format(args["model"]))
-preds = model.predict(image)
-P = imagenet_utils.decode_predictions(preds)
+model = InceptionV3(weights='imagenet', include_top=False, pooling='avg')
+features = extract_features(model, image299, preprocess);
 
-# loop over the predictions and display the rank-5 predictions +
-# probabilities to our terminal
-for (i, (imagenetID, label, prob)) in enumerate(P[0]):
-	print("{}. {}: {:.2f}%".format(i + 1, label, prob * 100))
 
-# load the image via OpenCV, draw the top prediction on the image,
-# and display the image to our screen
-#orig = cv2.imread(args["image"])
-#(imagenetID, label, prob) = P[0][0]
-#cv2.putText(orig, "Label: {}, {:.2f}%".format(label, prob * 100),
-#	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
-#cv2.imshow("Classification", orig)
-#cv2.waitKey(0)
+
+
